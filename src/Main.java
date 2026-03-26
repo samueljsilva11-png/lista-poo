4182e5684f47ef40a254a8d69cbeead130a2d54a
Samuel <samuel.j.silva11@aluno.senai.br>
2026-03-26 15:39:28 -0300

    exercicio 2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-
         carro carro1 = new carro();
         carro carro2 = new carro();
-
 
         carro1.marca = "Toyota";
         carro1.modelo = "Corolla";
@@ -16,13 +14,13 @@ public class Main {
 
 
         System.out.println("Carro 1:");
-        System.out.println("Marca: " + carro1.marca);
-        System.out.println("Modelo: " + carro1.modelo);
-        System.out.println("Ano: " + carro1.ano);
+        carro1.exibir();
 
         System.out.println("\nCarro 2:");
-        System.out.println("Marca: " + carro2.marca);
-        System.out.println("Modelo: " + carro2.modelo);
-        System.out.println("Ano: " + carro2.ano);
+        carro2.exibir();
+
+        System.out.println("\nIdade do carro 1: " + carro1.idadeDoCarro());
+        System.out.println("Idade do carro 2: " + carro2.idadeDoCarro());
+
     }
 }
--- src/carro.java
+++ src/carro.java
@@ -4,4 +4,14 @@ public class carro {
     String modelo;
     int ano;
 
+
+    void exibir() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Ano: " + ano);
+    }
+
+    int idadeDoCarro() {
+        return 2026 - ano;
+    }
 }
